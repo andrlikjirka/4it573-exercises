d77e0145a323ecbece54baa6df4d490e8a5a6cbb
andrlikjirka <andrlik.jirka@gmail.com>
2024-02-17 22:27:40 +0100

    add semicolon

--- exercise01/index.js
+++ exercise01/index.js
@@ -3,22 +3,23 @@ function guessNumber(randomNumber, maxAttempts) {
         let guess = parseInt(prompt('Jaké číslo si myslím?'));
 
         if (isNaN(guess)) {
-            console.log('Zadej platné číslo!')
+            console.log('Zadej platné číslo!');
+            continue;
         }
         if (guess === randomNumber) {
-            alert(guess + ' je to číslo.')
-            console.log('Gratulace! Uhádl jsi číslo ' + guess + ".")
+            alert(guess + ' je to číslo.');
+            console.log('Gratulace! Uhádl jsi číslo ' + guess + ".");
             return;
         } else {
-            alert(guess + ' není to číslo.\n' + 'Zkus hádat znovu!')
+            alert(guess + ' není to číslo.\n' + 'Zkus hádat znovu!');
         }
     }
-    console.log('Číslo jsi bohužel neuhádl.')
+    console.log('Číslo jsi bohužel neuhádl.');
 }
 
 function main() {
     const MAX_ATTEMPTS = 5;
-    
+
     let randomNumber = Math.floor((Math.random() * 10) + 1);
     console.log('Myslím si číslo.');
     
